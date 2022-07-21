9f671bb1b45a2f3c0f2ab5f7521b3f8f211fc5ee
imcrisam <imcrisam@gmail.com>
2022-07-21 09:53:16 -0500

    add ref in log

--- src/reactions/actions.js
+++ src/reactions/actions.js
@@ -7,25 +7,35 @@ module.exports = {
         const idNewPlayer = interaction.message.embeds[0].author.name;
         const member = await Discordjs.getMemberById(Client, idNewPlayer, interaction.message.guildId)
         const addedRoll = await Discordjs.addRolesMember(member, config.memberRol)
+
         if (addedRoll) {
             Discordjs.changeNickName(member, "[M.D.B]", interaction.message.embeds[0].title)
             interaction.message.reply('OK')
                 .then(msg => {
                     Discordjs.deleteMsg(interaction.message, 3500);
                     Discordjs.deleteMsg(msg, 4000);
-                   
+
                 })
-            return true;
-        }else{
+            return {
+                'resut': true,
+                'description': interaction.message.embeds[0].fields[0].value
+            };
+        } else {
             console.log("fail add rol");
-            return false;
+            return {
+                'resut': false,
+                'description': "fail add rol"
+            };
         }
     },
 
-    delete: async(Client, interaction, user, config) => {
-        Discordjs.respondeTemp(interaction.message, "delete", 3000,1000)
-        return true;
-        
+    delete: async (Client, interaction, user, config) => {
+        Discordjs.respondeTemp(interaction.message, "delete", 3000, 1000)
+        return {
+            'successful': true,
+            'description': ""
+        };
+
     }
 
 }
--- src/reactions/channel.js
+++ src/reactions/channel.js
@@ -5,43 +5,43 @@ module.exports = {
 
 	init: async (Client, interaction, user, config) => {
 
-		if(!config.reactions){
+		if (!config.reactions) {
 			console.log("conf reaction not found");
 			return
-		} 
+		}
 		const channelID = interaction.message.channelId;
 
 		const channel = await Client.channels.cache.get(channelID);
 		const msg = await channel.messages.fetch(interaction.message.id)
 
 		const content = msg.content.split(" ");
-		console.log(content);
-
 		const confiReaction = content[0] ? config.reactions[content[0].toLowerCase()][interaction._emoji.name] : undefined
-		if (!confiReaction){
+		if (!confiReaction) {
 			console.log("conf reaction.action not found");
 			return
-		} 
+		}
 
 		if (!Actions.hasOwnProperty(confiReaction.action)) {
 			console.log("action not found");
 			return
-		} 
+		}
 
-		let rolesUser =  await Discordjs.getRolesById(Client, user.id, interaction.message.guildId);
-		
-		if(!confiReaction.allowedRoles) {
+		let rolesUser = await Discordjs.getRolesById(Client, user.id, interaction.message.guildId);
+
+		if (!confiReaction.allowedRoles) {
 			console.log("allowedRoles not found");
 			return
-		} 
+		}
 		confiReaction.allowedRoles.forEach((rol) => {
-			if(rolesUser.includes(rol)){
+			if (rolesUser.includes(rol)) {
 				console.log(rol, " : ", confiReaction.action, " - ", Actions.hasOwnProperty(confiReaction.action));
-				const successful = Actions[confiReaction.action](Client, interaction, user, confiReaction)
-				console.log(user);
-				if(successful){
-					channel.send("<@"+user.id+">  "+confiReaction.description+" "+content[1])
-				}
+				Actions[confiReaction.action](Client, interaction, user, confiReaction).then((res) => {
+						
+					if (res.resut) {
+						channel.send("<@" + user.id + ">  " + confiReaction.description + " " + content[1] + "  ref{ " + res.description + "} ")
+					}
+				})
+
 				return;
 			}
 		})
